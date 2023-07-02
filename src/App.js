112d91883e2bed961b21b58d6307f4f1d89e4a7a
Techvel <138348258+Techvel@users.noreply.github.com>
2023-07-02 10:45:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+import Login from "./Components/login component";
+import Signup from "./Components/signup Components";
+import UserDetails from "./Components/UserDetails";
+
+
+function App() {
+  const isLoggedIn = window.localStorage.getItem("loggedIn");
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={isLoggedIn === "true" ? <UserDetails/> : <Login/>}
+          />
+          <Route path="/sign-in" element={<Login/>} />
+          <Route path="/sign-up" element={<Signup/>} />
+          <Route path="/userDetails" element={<UserDetails/>} />
+        </Routes>
+        {/* {<ImageUpload/>} */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
